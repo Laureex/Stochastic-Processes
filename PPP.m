1b060af26e5b9a1f11364103fce9a184db125378
Laura Xia <xiajiangyue@163.com>
2015-01-30 21:34:36 +0800

    Create PPP.m

---
+++ PPP.m
@@ -0,0 +1,6 @@
+lambda = @(x) 300*(x(:,1).^2 + x(:,2).^2);
+lamstar = 600;
+N=poissrnd(lamstar); x = rand(N,2); % homogeneous PP
+ind = find(rand(N,1) < lambda(x)/lamstar);
+xa = x(ind,:); % thinned PP
+plot(xa(:,1),xa(:,2))
