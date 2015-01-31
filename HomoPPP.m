64c77def412a28943d83e959ad56b2bd8e495f3d
Laura Xia <xiajiangyue@163.com>
2015-01-31 20:03:31 +0800

    Update HomoPPP.m

--- HomoPPP.m
+++ HomoPPP.m
@@ -1,7 +1,8 @@
 % create a homogeneous Poisson Point Process
 % lambda = @(x) 300*(x(:,1).^2 + x(:,2).^2);
+lambda = 300;
 lamstar = 600;
 N=poissrnd(lamstar); x = rand(N,2); % homogeneous PP
-ind = find(rand(N,1) < lambda(x)/lamstar);
+ind = find(rand(N,1) < lambda/lamstar);
 xa = x(ind,:); % thinned PP
 plot(xa(:,1),xa(:,2))
